f3466e4924cb90e617ac51899e4d2b9b04ceab13
Wanako0609 <84190429+Wanako0609@users.noreply.github.com>
2022-01-10 23:23:49 +0100

    patch page web
    la page ne fonctionnait pas important

--- page_web.py
+++ page_web.py
@@ -1,19 +1,46 @@
 #generation page web
-page = open("index.html","w+")
-title = "test2"
+# page = open("index.html","w+")
 
-page.write("<html>")
-page.write("<head>")
-page.write('<meta charset="utf-8" />')
-page.write("<title>")
-page.write( title )
-page.write("</title>")
-page.write("</head>")
-page.write("<body>")
-page.write("</body>")
-page.write("</html>")
 
-page.close
+# page.write("<html>")
+# page.write("<head>")
+# page.write('<meta charset="utf-8" />')
+# page.write("<title>")
+# page.write( title )
+# page.write("</title>")
+# page.write("</head>")
+# page.write("<body>")
+# page.write("<h2>")
+# page.write( h2 )
+# page.write("</h2>")
+# page.write("</body>")
+# page.write("</html>")
+# 
+# page.close
+
+
+title = "test"
+h2 = "titre"
+with open("index.html","w+") as file:
+    file.write("<html>")
+    #head
+    file.write("<head>")
+    file.write('<meta charset="utf-8" />')
+    file.write("<title>")
+    file.write( title )
+    file.write("</title>")
+    file.write("</head>")
+    #body
+    file.write("<body>")
+    file.write("<h2>")
+    file.write( h2 )
+    file.write("</h2>")
+    file.write("</body>")
+    
+    file.write("</html>")
+    
+    file.close
+    
 
 
 
